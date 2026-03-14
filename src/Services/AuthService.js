df45c6e9d91fa90245b78b0be4ac748bc83d0b66
TejasPatil-1947 <tejpatil001947@gmail.com>
2026-03-14 18:07:12 +0530

    Modified security checks

--- src/Services/AuthService.js
+++ src/Services/AuthService.js
@@ -19,9 +19,14 @@ const getToken = () => localStorage.getItem("token");
 /* ================= REQUEST INTERCEPTOR ================= */
 
 api.interceptors.request.use((config) => {
+
   const token = localStorage.getItem("token");
 
-  if (token) {
+  const isAuthRequest =
+    config.url.includes("/auth/login") ||
+    config.url.includes("/auth/register");
+
+  if (token && !isAuthRequest) {
     config.headers.Authorization = `Bearer ${token}`;
   }
 
